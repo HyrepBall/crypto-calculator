cd78f3c4fb10ebfd4b376e279e1b0c471d6a8058
Voronko <unglory44@gmail.com>
2019-02-20 23:04:05 +0200

    Updates

---
+++ precache-manifest.2f1125e7d125710248e366ba26254808.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "09de9e87407be9ef245d",
+    "url": "/crypto-calculator/static/js/runtime~main.09de9e87.js"
+  },
+  {
+    "revision": "0626dc2dc5244234bb57",
+    "url": "/crypto-calculator/static/js/main.0626dc2d.chunk.js"
+  },
+  {
+    "revision": "bcd3d414069abc9897ee",
+    "url": "/crypto-calculator/static/js/2.bcd3d414.chunk.js"
+  },
+  {
+    "revision": "0626dc2dc5244234bb57",
+    "url": "/crypto-calculator/static/css/main.4a38e7e8.chunk.css"
+  },
+  {
+    "revision": "bcd3d414069abc9897ee",
+    "url": "/crypto-calculator/static/css/2.7ff5b16c.chunk.css"
+  },
+  {
+    "revision": "2ac760f4c3cf00e42668191705a2c9fc",
+    "url": "/crypto-calculator/index.html"
+  }
+];
